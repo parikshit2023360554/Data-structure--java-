4c212888a913eff9980963f9f01555abf123070a
Parikshit singh <parikshitsingh@Parikshits-MacBook-Air.local>
2025-03-20 13:27:46 +0530

    file added

---
+++ LinkList.java
@@ -0,0 +1,44 @@
+class LinkList{
+    Node head; // Add head reference
+    class Node{
+        String data; 
+        Node next; 
+
+        Node(String data){
+            this.data=data;
+            this.next= null; 
+        }
+    }
+
+    public void PrintList(){
+        if(head==null){
+            System.out.println("list is empty");
+            return; 
+
+        }
+        Node currNode = head; 
+        while(currNode!=null){
+            System.out.print(currNode.data + "->");
+            currNode = currNode.next;
+        }
+
+    }
+
+    public void AddFirst(String data){
+        Node newNode = new Node(data);
+        if(head==null){
+            head = newNode;
+            return;
+        }
+        newNode.next = head ; 
+        head = newNode;
+    }
+
+
+    public static void main(String[] args){
+        LinkList list = new LinkList();
+        list.AddFirst("a");
+        list.AddFirst("b");
+        list.PrintList();
+    }
+}
